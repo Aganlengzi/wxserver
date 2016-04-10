4059c8735ecc4c4b3df9f467532b3c8c85fbebeb
Aganlengzi <jchsong@mail.ustc.edu.cn>
2016-04-10 22:03:03 +0800

    cover the Token and APIKEY of robot

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "IHKelzX8l6K45LlIL0s84b26536s432u";
+	public static final String TOKEN = "********";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = "fee7e46fd932211371a4f1c0686ddc3d"; 
+	public static final String APIKEY = "********"; 
 	
 }
